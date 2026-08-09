522bc25ac333d413860bfc046138c74ac1aae80a
高峰 <gaofeng2134@outlook.com>
2026-08-09 18:16:01 +0800

    添加串口 RTU 服务器示例 SimpleServer

---
+++ examples/typescript/serial/SimpleServer.ts
@@ -0,0 +1,23 @@
+import { IModbusServerOptions, ModbusRTUServer } from '../../../src/modbus'
+import { SerialStream } from './SerialStream'
+
+const options: IModbusServerOptions = {
+  id: 1,
+  coils: null,
+  discrete: null,
+  holding: Buffer.alloc(100),
+  input: Buffer.alloc(100)
+}
+
+const port = new SerialStream('COM1', 19200, 'none')
+const server = new ModbusRTUServer(port, options)
+port.open((error) => {
+  if (error) {
+    console.error('Failed to open serial port:', error)
+    return
+  }
+})
+server.on('connection', (client) => {
+  console.log('Client connected:', client.socket.portName)
+})
+
